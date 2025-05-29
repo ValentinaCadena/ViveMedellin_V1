3b5d8dbd8a2f6851b3225405b0a8ef91f3b3399a
Valentina Cadena <valentina.cadenaz@udea.edu.co>
2025-05-29 14:53:28 -0500

    Corrección en componentes eliminados y sin uso

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home () {
     <div className="flex h-screen flex-col gap-20 justify-center items-center">
       <Button text="Iniciar sesión con Google" icon="devicon:google"  iconPosition="left" color="orange" widthIcon="24" className="space-x-2 px-2 font-semibold"></Button>
       <Button text="Iniciar sesión con Google" icon="devicon:google"  iconPosition="left" color="emerald" widthIcon="24" className="font-semibold"></Button>
+      <Button text="Iniciar sesión con Google" icon="devicon:google"  iconPosition="left" color="blue" widthIcon="24" className="font-semibold"></Button>
     </div>
   )
 }
