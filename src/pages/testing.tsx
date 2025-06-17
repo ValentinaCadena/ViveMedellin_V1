cf702b5f6163f3a1a3d1c885aabd2872ef5099ab
ValentinaCadena <valentina.cadenaz@udea.edu.co>
2025-06-16 21:32:14 -0500

    Eliminación de componentes innecesarios

--- src/pages/testing.tsx
+++
@@ -1,9 +0,0 @@
-import CreateGroupSidebar from "@/components/organisms/createGroupSidebar"
-
-export default function Testing () {
-    return (
-        <div className="bg-white flex justify-center items-center h-screen w-[30%]">
-            <CreateGroupSidebar></CreateGroupSidebar>
-        </div>
-    )
-}
